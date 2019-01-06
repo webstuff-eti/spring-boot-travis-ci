a83c9c3bfc8b890938e169fa2a723b0cc0cbf898
Tiago Tibães <tibaestiago@gmail.com>
2019-01-06 17:20:09 -0200

    projeto inicial criado

---
+++ MaximizeSeuTempo/src/main/java/br/eti/webstuff/api/MaximizeSeuTempoApplication.java
@@ -0,0 +1,14 @@
+package br.eti.webstuff.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaximizeSeuTempoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaximizeSeuTempoApplication.class, args);
+	}
+
+}
+
