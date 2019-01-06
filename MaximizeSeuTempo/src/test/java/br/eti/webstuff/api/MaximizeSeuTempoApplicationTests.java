7faf6173d1a4802b53ee823627942e3a2e416838
Tiago Tibães <tibaestiago@gmail.com>
2019-01-06 21:43:02 -0200

    testando integração com travis

--- MaximizeSeuTempo/src/test/java/br/eti/webstuff/api/MaximizeSeuTempoApplicationTests.java
+++ MaximizeSeuTempo/src/test/java/br/eti/webstuff/api/MaximizeSeuTempoApplicationTests.java
@@ -9,7 +9,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class MaximizeSeuTempoApplicationTests {
 	
-	//TODO: IMPLEMENTAR TESTES
 	@Test
 	public void contextLoads() {
 		
