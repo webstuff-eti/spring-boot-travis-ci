835a40f42ed9a30124a2bd4f04be58deebfca620
Tiago Tibães <tibaestiago@gmail.com>
2019-01-09 13:24:49 -0200

    implementação da classe de serviço do Funcionário

--- MaximizeSeuTempo/src/main/java/br/eti/webstuff/api/service/impl/FuncionarioServiceImpl.java
+++ MaximizeSeuTempo/src/main/java/br/eti/webstuff/api/service/impl/FuncionarioServiceImpl.java
@@ -1,5 +1,48 @@
 package br.eti.webstuff.api.service.impl;
 
-public class FuncionarioServiceImpl {
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.eti.webstuff.api.entities.Funcionario;
+import br.eti.webstuff.api.repositories.FuncionarioRepository;
+import br.eti.webstuff.api.service.FuncionarioService;
+
+@Service
+public class FuncionarioServiceImpl implements FuncionarioService {
+	
+	private static final Logger log  = LoggerFactory.getLogger(FuncionarioServiceImpl.class);
+	
+	@Autowired
+	private FuncionarioRepository  funcionarioRepository;
+	
+	
+
+	@Override
+	public Funcionario persistir(Funcionario funcionario) {
+		log.info("Persistindo Funcionário: {}", funcionario);
+		return this.funcionarioRepository.save(funcionario);
+	}
+
+	@Override
+	public Optional<Funcionario> buscarPorCpf(String cpf) {
+		log.info("Buscando funcionário pelo CPF {}", cpf);
+		return Optional.ofNullable(this.funcionarioRepository.findByCpf(cpf));
+	}
+
+	@Override
+	public Optional<Funcionario> buscarPorEmail(String email) {
+		log.info("Buscando funcionário pelo email {}", email);
+		return Optional.ofNullable(this.funcionarioRepository.findByEmail(email));
+	}
+
+	@Override
+	public Optional<Funcionario> buscarPorId(Long id) {
+		log.info("Buscando funcionário pelo IDl {}", id);
+		return Optional.ofNullable(this.funcionarioRepository.findOne(id));
+	}
 
 }
