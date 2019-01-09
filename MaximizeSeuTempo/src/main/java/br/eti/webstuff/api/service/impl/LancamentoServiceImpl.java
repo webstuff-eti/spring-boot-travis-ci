ccdfb5b6e160dcff9868ffe03e64c693c6121363
Tiago Tibães <tibaestiago@gmail.com>
2019-01-09 13:31:56 -0200

    implementação da classe de serviço de Lancamento

--- MaximizeSeuTempo/src/main/java/br/eti/webstuff/api/service/impl/LancamentoServiceImpl.java
+++ MaximizeSeuTempo/src/main/java/br/eti/webstuff/api/service/impl/LancamentoServiceImpl.java
@@ -1,5 +1,48 @@
 package br.eti.webstuff.api.service.impl;
 
-public class LancamentoServiceImpl {
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import br.eti.webstuff.api.entities.Lancamento;
+import br.eti.webstuff.api.repositories.LancamentoRepository;
+import br.eti.webstuff.api.service.LancamentoService;
+
+@Service
+public class LancamentoServiceImpl implements LancamentoService {
+
+	private static final Logger log = LoggerFactory.getLogger(LancamentoServiceImpl.class);
+
+	@Autowired
+	private LancamentoRepository lancamentoRepository;
+
+	@Override
+	public Page<Lancamento> buscarPorFuncionarioId(Long funcionarioId, PageRequest pageRequest) {
+		log.info("Buscando lançamentos para o funcionário com ID {}", funcionarioId);
+		return this.lancamentoRepository.findByFuncionarioId(funcionarioId, pageRequest);
+	}
+
+	@Override
+	public Optional<Lancamento> buscarPorId(Long id) {
+		log.info("Buscando um lançamento pelo ID {}", id);
+		return Optional.ofNullable(this.lancamentoRepository.findOne(id));
+	}
+
+	@Override
+	public Lancamento persistir(Lancamento lancamento) {
+		log.info("Persistindo o lançamento: {}", lancamento);
+		return this.lancamentoRepository.save(lancamento);
+	}
+
+	@Override
+	public void remover(Long id) {
+		log.info("Excluindo o lançamento ID {}", id);
+		this.lancamentoRepository.delete(id);
+	}
 
 }
