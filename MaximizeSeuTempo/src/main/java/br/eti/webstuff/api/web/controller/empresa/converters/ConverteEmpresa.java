66bdbaf790346b2372f22f4820d579016955842c
Tiago Tibães <tibaestiago@gmail.com>
2019-01-10 19:00:05 -0200

    implementação da conversão do objeto DTO para OBJETO

--- MaximizeSeuTempo/src/main/java/br/eti/webstuff/api/web/controller/empresa/converters/ConverteEmpresa.java
+++ MaximizeSeuTempo/src/main/java/br/eti/webstuff/api/web/controller/empresa/converters/ConverteEmpresa.java
@@ -6,20 +6,41 @@ import br.eti.webstuff.api.web.dto.EmpresaDto;
 public class ConverteEmpresa {
 	
 	/**
-	 * Converte Converte Empresa para EmpresaDto.
+	 * Converte Empresa para EmpresaDto.
 	 * 
 	 * @param empresa
-	 * @return 
+	 * 
 	 * @return EmpresaDto
 	 */
 	 public EmpresaDto converteEmpresaParaEmpresaDto(Empresa empresa) {
 		
 		EmpresaDto empresaDto = new EmpresaDto();
-		empresaDto.setId(empresa.getId());
-		empresaDto.setCnpj(empresa.getCnpj());
-		empresaDto.setRazaoSocial(empresa.getRazaoSocial());
-
+		
+		if(empresa != null) {
+			empresaDto.setId(empresa.getId());
+			empresaDto.setCnpj(empresa.getCnpj());
+			empresaDto.setRazaoSocial(empresa.getRazaoSocial());
+		}
 		return empresaDto;
+	}
+	 
+	 /**
+		 * Converte EmpresaDto para Empresa.
+		 * 
+		 * @param EmpresaDto
+		 *
+		 * @return empresa
+		 */
+	public Empresa converteEmpresaDtoParaEmpresa(EmpresaDto empresaDto) {
+		
+		Empresa empresa = new Empresa();
+		
+		empresa.setCnpj(empresaDto.getCnpj());
+		empresa.setRazaoSocial(empresaDto.getRazaoSocial());
+		if(empresaDto.getId() != null) {
+			empresa.setId(empresaDto.getId());
+		}
+		return empresa;
 	}
 	
 	
