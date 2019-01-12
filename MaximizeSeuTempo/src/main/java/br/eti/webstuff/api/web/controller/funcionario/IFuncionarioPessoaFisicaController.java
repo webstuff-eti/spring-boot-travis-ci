915d5127b6850e9e32e3a588fcf4ccbfc81abba5
Tiago Tibães <tibaestiago@gmail.com>
2019-01-12 14:56:56 -0200

    implementação da interface da classe FuncionarioPessoaFisicaController

--- MaximizeSeuTempo/src/main/java/br/eti/webstuff/api/web/controller/funcionario/IFuncionarioPessoaFisicaController.java
+++ MaximizeSeuTempo/src/main/java/br/eti/webstuff/api/web/controller/funcionario/IFuncionarioPessoaFisicaController.java
@@ -6,10 +6,13 @@ import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import br.eti.webstuff.api.web.controller.responses.Response;
+import br.eti.webstuff.api.web.dto.request.EmpresaRequestDto;
 import br.eti.webstuff.api.web.dto.request.PessoaFisicaRequestDto;
+import br.eti.webstuff.api.web.dto.response.EmpresaResponseDto;
 import br.eti.webstuff.api.web.dto.response.PessoaFisicaResponseDto;
 
 public interface IFuncionarioPessoaFisicaController {
@@ -17,5 +20,11 @@ public interface IFuncionarioPessoaFisicaController {
 	
 	public ResponseEntity<Response<PessoaFisicaResponseDto>> cadastrarPessoaFisica(@Valid @RequestBody PessoaFisicaRequestDto pessoaFisicaRequestDto,
 			BindingResult result) throws NoSuchAlgorithmException;
+	
+	public ResponseEntity<Response<PessoaFisicaResponseDto>> buscarPorCpf(@PathVariable("cpf") String documento);
+	
+	
+	public ResponseEntity<Response<PessoaFisicaResponseDto>> atualizarPessoaFisicaById(@PathVariable("id") Long id,
+			@Valid @RequestBody PessoaFisicaRequestDto pessoaFisicaRequestDto, BindingResult result) throws NoSuchAlgorithmException;
 
 }
