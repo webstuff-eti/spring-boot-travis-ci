e2fa6268707fb6123c3b55dae6215131a4307cc3
Tiago Tibães <tibaestiago@gmail.com>
2019-01-12 14:09:38 -0200

    iniciando implementacao do controller para Funcionários

---
+++ MaximizeSeuTempo/src/main/java/br/eti/webstuff/api/web/controller/funcionario/IFuncionarioPessoaFisicaController.java
@@ -0,0 +1,21 @@
+package br.eti.webstuff.api.web.controller.funcionario;
+
+import java.security.NoSuchAlgorithmException;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import br.eti.webstuff.api.web.controller.responses.Response;
+import br.eti.webstuff.api.web.dto.request.PessoaFisicaRequestDto;
+import br.eti.webstuff.api.web.dto.response.PessoaFisicaResponseDto;
+
+public interface IFuncionarioPessoaFisicaController {
+	
+	
+	public ResponseEntity<Response<PessoaFisicaResponseDto>> cadastrarPessoaFisica(@Valid @RequestBody PessoaFisicaRequestDto pessoaFisicaRequestDto,
+			BindingResult result) throws NoSuchAlgorithmException;
+
+}
