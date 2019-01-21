bbefbe7cafd735cf67d7369d34577ba74fe4d3a0
Tiago Poliano Tibaes <tptibaes@stefanini.com>
2019-01-21 18:27:36 -0200

    iniciando atualização da classe de controle genérico para Funcionario

---
+++ MaximizeSeuTempo/src/main/java/br/eti/webstuff/api/web/controller/funcionario/FuncionarioController.java
@@ -0,0 +1,13 @@
+package br.eti.webstuff.api.web.controller.funcionario;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/funcionarios")
+@CrossOrigin("*")
+public class FuncionarioController {
+
+}
